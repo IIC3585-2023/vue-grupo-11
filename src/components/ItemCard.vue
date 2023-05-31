2d7afdaf3db9d02b7bbf28b9594a49f08dfc427b
Fernando <fjquintana@uc.cl>
2023-05-30 21:09:18 -0400

    Add first card implementation

---
+++ src/components/ItemCard.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { CCard, CCardImage, CCardBody, CCardTitle, CCardText,
+        CCardSubtitle, CCardHeader, CButton} from "@coreui/vue";
+import '@coreui/coreui/dist/css/coreui.min.css'
+import { ref } from 'vue'
+
+const props = defineProps({
+    id: Number,
+    title: String,
+    imgURL: String,
+    description: String,
+    author: String,
+    authorId: Number,
+    category: String,
+    amount: Number,
+    campus: String,
+    favourite: Boolean,
+});
+
+const {id, title, imgURL, description, author, category, amount, campus, authorId} = props;
+
+const favourite = ref(props.favourite);
+
+const toggleFavourite = () => {
+    favourite.value = !favourite.value
+    //TODO: Agregar funcion para mantener los favoritos en el store.
+    //      Habria que agregar el ID o el item completo cuando se ahce favourite,
+    //      y eliminarlo cuando se hace un-favourite
+}
+
+const buyItem = () => {
+    //TODO: Hacer que esta funcion llame a la api/haga lo que tenga que hacer
+    console.log(`Bought item ${id}!`)
+}
+
+const contactAuthor = () => {
+    //TODO: Hacer que esta funcion haga lo que tiene que hacer
+    console.log(`Contacted ${author} (ID: ${authorId})!`)
+}
+</script>
+
+
+<template>
+    <CCard style="max-width: 25rem">
+        <CCardHeader>
+            <div class="d-flex justify-content-between">
+                {{ author }}
+                <img :src="[favourite ? 'src/assets/star-fill.svg' : 'src/assets/star.svg']" v-on:click="toggleFavourite"/>
+            </div>
+            {{ campus }}
+        </CCardHeader>
+        <CCardImage orientation="top" v-bind:src="imgURL" />
+        <CCardBody>
+            <CCardTitle>{{ title }}</CCardTitle>
+            <CCardSubtitle class="mb-2">{{ category }}</CCardSubtitle>
+            <CCardText>{{ description }}</CCardText>
+            <CCardTitle>{{ new Intl.NumberFormat('es-cl', { style: 'currency', currency: 'CLP' }).format(amount) }}</CCardTitle>
+            <div class="d-flex justify-content-between">
+                <CButton color="success" variant="outline" v-on:click="buyItem">Comprar</CButton>
+                <CButton color="info" variant="outline" v-on:click="contactAuthor">Contacto</CButton>
+            </div>
+        </CCardBody>
+    </CCard>
+</template>
+
+<style scoped>
+.card {
+    margin: 20px
+}
+</style>
---
+++ src/views/ItemsView.vue
@@ -0,0 +1,171 @@
+<script setup>
+import ItemCard from '../components/ItemCard.vue';
+import {CRow, CCol} from '@coreui/vue'
+
+// TODO: Llamar a la api para conseguir estos props
+const cardProps = [
+    {
+        id: 1,
+        title: 'Brownies veganos',
+        imgURL: 'https://img.taste.com.au/gOpVgvz8/taste/2016/11/classic-chewy-brownie-102727-1.jpeg',
+        description: 'Brownies exquisitos muito bom para comprar 9.990 con tarjeta CMR.',
+        author: 'roro47',
+        category: 'Comida',
+        amount: 993450,
+        campus: 'San Joaquin',
+        favourite: false,
+        authorId: 1
+    }, {
+        id: 1,
+        title: 'Brownies veganos',
+        imgURL: 'https://img.taste.com.au/gOpVgvz8/taste/2016/11/classic-chewy-brownie-102727-1.jpeg',
+        description: 'Brownies exquisitos muito bom para comprar 9.990 con tarjeta CMR.',
+        author: 'roro47',
+        category: 'Comida',
+        amount: 993450,
+        campus: 'San Joaquin',
+        favourite: false,
+        authorId: 1
+    }, {
+        id: 1,
+        title: 'Brownies veganos',
+        imgURL: 'https://img.taste.com.au/gOpVgvz8/taste/2016/11/classic-chewy-brownie-102727-1.jpeg',
+        description: 'Brownies exquisitos muito bom para comprar 9.990 con tarjeta CMR.',
+        author: 'roro47',
+        category: 'Comida',
+        amount: 993450,
+        campus: 'San Joaquin',
+        favourite: false,
+        authorId: 1
+    }, {
+        id: 1,
+        title: 'Brownies veganos',
+        imgURL: 'https://img.taste.com.au/gOpVgvz8/taste/2016/11/classic-chewy-brownie-102727-1.jpeg',
+        description: 'Brownies exquisitos muito bom para comprar 9.990 con tarjeta CMR.',
+        author: 'roro47',
+        category: 'Comida',
+        amount: 993450,
+        campus: 'San Joaquin',
+        favourite: false,
+        authorId: 1
+    }, {
+        id: 1,
+        title: 'Brownies veganos',
+        imgURL: 'https://img.taste.com.au/gOpVgvz8/taste/2016/11/classic-chewy-brownie-102727-1.jpeg',
+        description: 'Brownies exquisitos muito bom para comprar 9.990 con tarjeta CMR.',
+        author: 'roro47',
+        category: 'Comida',
+        amount: 993450,
+        campus: 'San Joaquin',
+        favourite: false,
+        authorId: 1
+    }, {
+        id: 1,
+        title: 'Brownies veganos',
+        imgURL: 'https://img.taste.com.au/gOpVgvz8/taste/2016/11/classic-chewy-brownie-102727-1.jpeg',
+        description: 'Brownies exquisitos muito bom para comprar 9.990 con tarjeta CMR.',
+        author: 'roro47',
+        category: 'Comida',
+        amount: 993450,
+        campus: 'San Joaquin',
+        favourite: false,
+        authorId: 1
+    }, {
+        id: 1,
+        title: 'Brownies veganos',
+        imgURL: 'https://img.taste.com.au/gOpVgvz8/taste/2016/11/classic-chewy-brownie-102727-1.jpeg',
+        description: 'Brownies exquisitos muito bom para comprar 9.990 con tarjeta CMR.',
+        author: 'roro47',
+        category: 'Comida',
+        amount: 993450,
+        campus: 'San Joaquin',
+        favourite: false,
+        authorId: 1
+    }, {
+        id: 1,
+        title: 'Brownies veganos',
+        imgURL: 'https://img.taste.com.au/gOpVgvz8/taste/2016/11/classic-chewy-brownie-102727-1.jpeg',
+        description: 'Brownies exquisitos muito bom para comprar 9.990 con tarjeta CMR.',
+        author: 'roro47',
+        category: 'Comida',
+        amount: 993450,
+        campus: 'San Joaquin',
+        favourite: false,
+        authorId: 1
+    }, {
+        id: 1,
+        title: 'Brownies veganos',
+        imgURL: 'https://img.taste.com.au/gOpVgvz8/taste/2016/11/classic-chewy-brownie-102727-1.jpeg',
+        description: 'Brownies exquisitos muito bom para comprar 9.990 con tarjeta CMR.',
+        author: 'roro47',
+        category: 'Comida',
+        amount: 993450,
+        campus: 'San Joaquin',
+        favourite: false,
+        authorId: 1
+    }, {
+        id: 2,
+        title: 'Brownies veganos mejores',
+        imgURL: 'https://img.taste.com.au/gOpVgvz8/taste/2016/11/classic-chewy-brownie-102727-1.jpeg',
+        description: 'Brownies exquisitos muito bom para comprar 9.990 con tarjeta CMR. Mejor que los de arriba',
+        author: 'roro47',
+        category: 'Comida',
+        amount: 99345,
+        campus: 'San Joaquin',
+        favourite: false,
+        authorId: 1
+    }, {
+        id: 2,
+        title: 'Brownies veganos mejores',
+        imgURL: 'https://img.taste.com.au/gOpVgvz8/taste/2016/11/classic-chewy-brownie-102727-1.jpeg',
+        description: 'Brownies exquisitos muito bom para comprar 9.990 con tarjeta CMR. Mejor que los de arriba',
+        author: 'roro47',
+        category: 'Comida',
+        amount: 99345,
+        campus: 'San Joaquin',
+        favourite: false,
+        authorId: 1
+    }, {
+        id: 2,
+        title: 'Brownies veganos mejores',
+        imgURL: 'https://img.taste.com.au/gOpVgvz8/taste/2016/11/classic-chewy-brownie-102727-1.jpeg',
+        description: 'Brownies exquisitos muito bom para comprar 9.990 con tarjeta CMR. Mejor que los de arriba',
+        author: 'roro47',
+        category: 'Comida',
+        amount: 99345,
+        campus: 'San Joaquin',
+        favourite: false,
+        authorId: 1
+    }, {
+        id: 2,
+        title: 'Brownies veganos mejores',
+        imgURL: 'https://img.taste.com.au/gOpVgvz8/taste/2016/11/classic-chewy-brownie-102727-1.jpeg',
+        description: 'Brownies exquisitos muito bom para comprar 9.990 con tarjeta CMR. Mejor que los de arriba',
+        author: 'roro47',
+        category: 'Comida',
+        amount: 99345,
+        campus: 'San Joaquin',
+        favourite: false,
+        authorId: 1
+    }, 
+]
+</script>
+
+<template>
+    <CRow :xs="{cols: 1}" :md="{cols: 2}" :lg="{cols: 3}" :xxl="{cols: 4}">
+        <CCol xs v-for="prop in cardProps" class="d-flex justify-content-center">
+            <ItemCard 
+                :id="prop.id"
+                :title="prop.title"
+                :imgURL="prop.imgURL"
+                :description="prop.description"
+                :author="prop.author"
+                :category="prop.category"
+                :amount="prop.amount"
+                :campus="prop.campus"
+                :favourite="prop.favourite"
+                :authorId="prop.authorId" 
+            />
+        </CCol>
+    </CRow>
+</template>
